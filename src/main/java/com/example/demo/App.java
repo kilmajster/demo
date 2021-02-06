8d9c05ba1c7856bd12791a4ba0d5047e663c1bd8
kilmajster <lukasz.createam@gmail.com>
2021-02-06 22:10:06 +0100

    v0.3.0

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class DemoApplication {
+public class App {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 	public @GetMapping("/") String sayHello() {
